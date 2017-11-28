555de1cdd4db1f47e4f5a2966a2dbedf03080976
Phillip Neiman <pneiman1@gmail.com>
2017-11-27 16:21:22 -0800

    Ready for awesome

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 
 // Put all API endpoints under '/api'
 app.get('/api/passwords', (req, res) => {
-    const count = 10;
+    const count = 12;
 
     // Generate some passwords
     const passwords = Array.from(Array(count).keys()).map(i =>
